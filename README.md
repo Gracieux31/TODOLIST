0fa801d0a1f4870d9cecbaee92ac1115a7024888
Gracieux <ahlonsougracieux@gmail.com>
2022-01-20 15:41:02 +0100

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # TODOLIST
+
+- Faire un php -S localhost:8001 ddans le terminal pour lancer le server ou copier le dossier dans votre server(wamp,xamp,mamp)
+
+#*Cet Projet consiste a mettre en place une APPtodolist en POO*
+Au cours de developpement de cet projet nous avons : <br>
+->connection/ <br>
+->controller/  <br>
+->model/       <br>
+->public/      <br>
+->routes       <br>
+->view/template/
+
+Un MVC biens structuré pour la bonne compréhensoon
+
+Tâches resant : 
+<li> Cookies
+<li> verification des users
+<li> TodoList
+
--- view/dashboard.php
+++ view/dashboard.php
@@ -7,30 +7,7 @@
 <body>
         <?php include_once('template/navigation.php'); ?>
 
-<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-            <a class="navbar-brand" href="#">TodoList</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Paramètre</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="?action=logout">LogOut</a>
-            </li>
-        </ul>
-    </div>
-  </div>
-</nav> -->
+
         <div class="position-relative"> <br> <br> <br>
             <div class="position-absolute top-50 start-50 translate-middle">
             Bienvenue sur notre Page ToDolist
